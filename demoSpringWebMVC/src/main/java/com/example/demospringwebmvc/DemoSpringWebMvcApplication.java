35ba8fced0c024063d108e7f8e1b7da127bc3074
BeobSik <tjswl756@naver.com>
2022-02-22 14:28:34 +0900

    init: dempSpringWebMVC project

---
+++ demoSpringWebMVC/src/main/java/com/example/demospringwebmvc/DemoSpringWebMvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.demospringwebmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringWebMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringWebMvcApplication.class, args);
+    }
+
+}
---
+++ demoSpringWebMVC/src/test/java/com/example/demospringwebmvc/DemoSpringWebMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demospringwebmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringWebMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
