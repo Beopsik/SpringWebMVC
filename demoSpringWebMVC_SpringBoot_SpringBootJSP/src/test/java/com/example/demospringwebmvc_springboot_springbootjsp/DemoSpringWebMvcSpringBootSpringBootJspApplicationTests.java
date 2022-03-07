83255e12cac31830f651c1a1d5bb5946fa60e7e8
BeobSik <tjswl756@naver.com>
2022-03-07 14:49:02 +0900

    feat: 스프링 부트 JSP

---
+++ demoSpringWebMVC_SpringBoot_SpringBootJSP/src/test/java/com/example/demospringwebmvc_springboot_springbootjsp/DemoSpringWebMvcSpringBootSpringBootJspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demospringwebmvc_springboot_springbootjsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringWebMvcSpringBootSpringBootJspApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
