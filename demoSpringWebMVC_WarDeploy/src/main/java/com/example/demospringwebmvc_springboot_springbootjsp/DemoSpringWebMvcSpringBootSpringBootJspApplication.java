bdb2ebb8a543b1b6445276442abb518d3b2f7103
BeobSik <tjswl756@naver.com>
2022-03-08 20:13:05 +0900

    feat: WAR 파일 배포하기

---
+++ demoSpringWebMVC_WarDeploy/src/main/java/com/example/demospringwebmvc_springboot_springbootjsp/DemoSpringWebMvcSpringBootSpringBootJspApplication.java
@@ -0,0 +1,13 @@
+package com.example.demospringwebmvc_springboot_springbootjsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringWebMvcSpringBootSpringBootJspApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringWebMvcSpringBootSpringBootJspApplication.class, args);
+    }
+
+}
