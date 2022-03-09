7a7c22f495eba660d2d6528c18f05308929a0d40
BeobSik <tjswl756@naver.com>
2022-03-09 11:11:30 +0900

    feat: spring boot formatter

--- demoSpringWebMVC_WebMvcConfigurer_Formatter/src/test/java/com/example/demospringwebmvc_webmvcconfigurer_formatter/SimpleControllerTest.java
+++ demoSpringWebMVC_WebMvcConfigurer_Formatter/src/test/java/com/example/demospringwebmvc_webmvcconfigurer_formatter/SimpleControllerTest.java
@@ -3,7 +3,9 @@ package com.example.demospringwebmvc_webmvcconfigurer_formatter;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -12,7 +14,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
+@SpringBootTest
+@AutoConfigureMockMvc
 public class SimpleControllerTest {
 
     @Autowired
