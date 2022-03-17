556d7788b6c62b83492f8ee7fc2a85bf1c1ac376
BeobSik <tjswl756@naver.com>
2022-03-17 15:49:30 +0900

    feat: 기본 리소스 핸들러

---
+++ demoSpringWebMVC_ResourceHandler/src/main/java/com/example/demospringwebmvc_webmvcconfigurer_formatter/WebConfig.java
@@ -0,0 +1,17 @@
+package com.example.demospringwebmvc_webmvcconfigurer_formatter;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new GreetingInterceptor()).order(0);
+        registry.addInterceptor(new AnotherInterceptor())
+                .addPathPatterns("/hi/**")
+                .order(-1);
+    }
+}
