a23d009a2d7c70aa71a41a38031ba39dceb80242
BeobSik <tjswl756@naver.com>
2022-07-18 20:22:53 +0900

    요청 맵핑하기 3부 미디어 타입

--- demoSpringWebMvcApplication/src/main/java/com/example/demospringwebmvcapplication/SimpleController.java
+++ demoSpringWebMvcApplication/src/main/java/com/example/demospringwebmvcapplication/SimpleController.java
@@ -2,14 +2,13 @@ package com.example.demospringwebmvcapplication;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.http.MediaType;
 
 @Controller
 public class SimpleController {
 
-    @GetMapping("hello/?")
+    @GetMapping(value = "/hello", consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public String hello(){
         return "hello";
--- demoSpringWebMvcApplication/src/test/java/com/example/demospringwebmvcapplication/SimpleControllerTest.java
+++ demoSpringWebMvcApplication/src/test/java/com/example/demospringwebmvcapplication/SimpleControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -19,7 +20,9 @@ public class SimpleControllerTest {
 
     @Test
     public void helloTest() throws Exception {
-        mockMvc.perform(get("/hello/1"))
+        mockMvc.perform(get("/hello")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().string("hello"));
