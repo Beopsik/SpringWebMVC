9456b3ed4a845077a34e17970614c1ded0f4c9e3
BeobSik <tjswl756@naver.com>
2022-07-18 19:07:13 +0900

    feat: 요청 맵핑하기 1부 HTTP Method

---
+++ demoSpringWebMvcApplication/src/test/java/com/example/demospringwebmvcapplication/DemoSpringWebMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demospringwebmvcapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringWebMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ demoSpringWebMvcApplication/src/test/java/com/example/demospringwebmvcapplication/SimpleControllerTest.java
@@ -0,0 +1,33 @@
+package com.example.demospringwebmvcapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class SimpleControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void helloTest() throws Exception {
+        mockMvc.perform(get("/hello"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("hello"));
+
+        mockMvc.perform(post("/hello"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("hello"));
+    }
+
+}
