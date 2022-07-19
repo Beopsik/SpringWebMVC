c5f6bee1afdfea77e38dd9607e6766ac480d7ddb
BeobSik <tjswl756@naver.com>
2022-07-19 14:53:53 +0900

    핸들러 메소드 4부 폼 서브밋

--- demoSpringWebMvcApplication/src/main/java/com/example/demospringwebmvcapplication/SimpleController.java
+++ demoSpringWebMvcApplication/src/main/java/com/example/demospringwebmvcapplication/SimpleController.java
@@ -1,12 +1,19 @@
 package com.example.demospringwebmvcapplication;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.http.MediaType;
 
 @Controller
 public class SimpleController {
+
+    @GetMapping("/events/form")
+    public String eventsForm(Model model){
+        Event newEvent = new Event();
+        newEvent.setId(1);
+        model.addAttribute("event", newEvent);
+        return "events/form";
+    }
 
     @PostMapping("events")
     @ResponseBody
