e9db64c4d95d692d2fcc295989c4acab60dda3e6
BeobSik <tjswl756@naver.com>
2022-07-22 10:39:42 +0900

    feat: 핸들러 메소드 14부 ResponseEntity

--- demoSpringWebMvcApplication/src/main/java/com/example/demospringwebmvcapplication/FileController.java
+++ demoSpringWebMvcApplication/src/main/java/com/example/demospringwebmvcapplication/FileController.java
@@ -1,16 +1,24 @@
 package com.example.demospringwebmvcapplication;
 
+import org.apache.tika.Tika;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.IOException;
+
 @Controller
 public class FileController {
+
+    @Autowired
+    private ResourceLoader resourceLoader;
 
     @GetMapping("/file")
     public String fileUploadForm(Model model){
@@ -23,5 +31,19 @@ public class FileController {
         String message = file.getOriginalFilename() + "is uploaded";
         attributes.addFlashAttribute("message", message);
         return "redirect:/file";
+    }
+
+    @GetMapping("/file/{filename}")
+    public ResponseEntity<Resource> fileDownload(@PathVariable String filename) throws IOException {
+        Resource resource = resourceLoader.getResource("classpath:" + filename);
+
+        Tika tika = new Tika();
+        String mediaType = tika.detect(resource.getFile());
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachement; filename=\"" + resource.getFilename() + "\"")
+                .header(HttpHeaders.CONTENT_TYPE, mediaType)
+                .header(HttpHeaders.CONTENT_LENGTH, String.valueOf(resource.contentLength()))
+                .body(resource);
     }
 }
